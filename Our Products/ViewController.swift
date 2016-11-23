52cf4644f25d6f4ca33ad0e710b162b8cabfb2ad
Tichafa Gava <tgava@Tichafas-MacBook-Pro.local>
2016-11-23 19:53:34 +0000

    Initial Commit

---
+++ Our Products/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Our Products
+//
+//  Created by Tichafa Gava on 23/11/2016.
+//  Copyright © 2016 Tichafa Gava - Ketts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
